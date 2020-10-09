57199a25afff50eebbdb17911b1c5d4d4fc80db6
danielgallo <daniel.gallo@gmail.com>
2020-10-09 14:20:12 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -48,7 +48,7 @@ object Build : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
-        root(DocumentationVcs, "+:. => temp")
+        root(DocumentationVcs, "+:. => temp/%Product%-documentation")
     }
 
     params {
